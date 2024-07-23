f68b14df1668b6057b7c6417fee186f11e1ed55f
Aziz Hmidi <azizhmidi125@gmail.com>
2024-07-23 12:18:14 +0100

    actual final fix this time

---
+++ docs/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GQTFFUK7.mjs";import"./chunk-5BBM26FN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-PPOSVPTZ.mjs";import"./chunk-5BBM26FN.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
